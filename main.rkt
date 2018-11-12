f05d83b999e64913ba71ae714051d6dc6a8292a7
Luis Borjas <luisfborjas@gmail.com>
2018-11-11 22:05:43 -0500

    adds more controls, and lambdas

--- main.rkt
+++ main.rkt
@@ -7,10 +7,48 @@
                           [width 500]
                           [height 500]))
 
+;; see: http://docs.racket-lang.org/gui/horizontal-panel_.html
+(define panel (new horizontal-panel% [parent frame]))
+
 ;;; From: http://docs.racket-lang.org/gui/editor-overview.html
-(define canvas (new editor-canvas% [parent frame]))
 (define editor (new text%))
+
+;;; TODO: move these to a module!
+(define is-markdown-filename?
+  (λ (filename)
+    (regexp-match #rx".*\\.md$" filename)))
+
+(define (get-markdown-files [project (current-directory)])
+  (filter is-markdown-filename? (directory-list project)))
+
+(define as-titles
+  (λ (paths)
+    (let ([remove-extension (λ (name)
+                              (string-replace name ".md" ""))])
+        (map (compose string-titlecase
+                      remove-extension
+                      path->string)
+             paths))))
+
+;; First child of `panel`, will be aligned to the left.
+;; see: http://docs.racket-lang.org/gui/list-box_.html
+(define project-files (new list-box%
+                           [label "Project Name"]
+                           [choices (as-titles (get-markdown-files))]
+                           [parent panel]
+                           [style '(vertical-label single)]
+                           [min-width 150]
+                           [stretchable-width #f]))
+
+
+;;; From: http://docs.racket-lang.org/gui/editor-overview.html
+;; second child of `panel`, will be aligned to the right
+(define canvas (new editor-canvas%
+                    [parent panel]
+                    [min-width 350]))
+
 (send canvas set-editor editor)
+
 
 ;; Menus n shit
 (define mb (new menu-bar% [parent frame]))
@@ -23,7 +61,7 @@
                     [label "Save"]
                     [parent m-file]
                     [shortcut #\s]
-                    [callback (lambda (x y)
+                    [callback (λ (x y)
                                 (send editor save-file "test.md"))]))
 (send editor set-max-undo-history 100)
 
@@ -32,5 +70,4 @@
  
 ; Show the frame by calling its show method
 (send frame show #t)
-
 
